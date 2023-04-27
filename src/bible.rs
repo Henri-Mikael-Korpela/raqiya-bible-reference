8544e77a8d7f984b112d3043a46b0c370e1d0b69
Henri Korpela <henri.korpela@vincit.fi>
2023-04-27 22:54:29 +0300

    Add all books of the Pentateuch for LSB and R1993/-38

--- src/bible.rs
+++ src/bible.rs
@@ -11,6 +11,13 @@ type BookHashMap = HashMap<BookId, (&'static str, u8)>;
 #[derive(Clone, Debug, Hash, Eq, PartialEq)]
 #[repr(u8)]
 pub enum BookId {
+    // All book IDs are listed in the canonical order,
+    // from the Old Testament to the New Testament.
+    Genesis,
+    Exodus,
+    Leviticus,
+    Numbers,
+    Deuteronomy,
     Matthew,
     John,
 }
@@ -41,19 +48,67 @@ impl BookInfo {
 
 lazy_static! {
     static ref BOOK_INFO_FOR_EN_LSB: BookHashMap = HashMap::from([
+        (BookId::Genesis, ("Genesis", 50)),
+        (BookId::Exodus, ("Exodus", 40)),
+        (BookId::Leviticus, ("Leviticus", 27)),
+        (BookId::Numbers, ("Numbers", 36)),
+        (BookId::Deuteronomy, ("Deuteronomy", 34)),
         (BookId::Matthew, ("Matthew", 28)),
         (BookId::John, ("John", 21)),
     ]);
     static ref BOOK_INFO_FOR_FI_R1933_38: BookHashMap = HashMap::from([
+        (BookId::Genesis, ("1. Moos.", 50)),
+        (BookId::Exodus, ("2. Moos.", 40)),
+        (BookId::Leviticus, ("3. Moos.", 27)),
+        (BookId::Numbers, ("4. Moos.", 36)),
+        (BookId::Deuteronomy, ("5. Moos.", 34)),
         (BookId::Matthew, ("Matt.", 28)),
         (BookId::John, ("Joh.", 21))
     ]);
     static ref BOOK_ABBREVIATIONS_TO_IDS_EN: HashMap<&'static str, BookId> = HashMap::from([
+        ("gen", BookId::Genesis),
+        ("genesis", BookId::Genesis),
+        ("gn", BookId::Genesis),
+        ("ex", BookId::Exodus),
+        ("exo", BookId::Exodus),
+        ("exodus", BookId::Exodus),
+        ("lev", BookId::Leviticus),
+        ("leviticus", BookId::Leviticus),
+        ("lv", BookId::Leviticus),
+        ("nm", BookId::Numbers),
+        ("num", BookId::Numbers),
+        ("numbers", BookId::Numbers),
+        ("de", BookId::Deuteronomy),
+        ("deu", BookId::Deuteronomy),
+        ("deuteronomy", BookId::Deuteronomy),
+        ("dt", BookId::Deuteronomy),
         ("matt", BookId::Matthew),
         ("matthew", BookId::Matthew),
+        ("mt", BookId::Matthew),
+        ("jh", BookId::John),
         ("john", BookId::John),
     ]);
     static ref BOOK_ABBREVIATIONS_TO_IDS_FI: HashMap<&'static str, BookId> = HashMap::from([
+        ("1mo", BookId::Genesis),
+        ("1 moos", BookId::Genesis),
+        ("1 mooses", BookId::Genesis),
+        ("1. moos.", BookId::Genesis),
+        ("2mo", BookId::Exodus),
+        ("2 moos", BookId::Exodus),
+        ("2 mooses", BookId::Exodus),
+        ("2. moos.", BookId::Exodus),
+        ("3mo", BookId::Leviticus),
+        ("3 moos", BookId::Leviticus),
+        ("3 mooses", BookId::Leviticus),
+        ("3. moos.", BookId::Leviticus),
+        ("4mo", BookId::Numbers),
+        ("4 moos", BookId::Numbers),
+        ("4 mooses", BookId::Numbers),
+        ("4. moos.", BookId::Numbers),
+        ("5mo", BookId::Deuteronomy),
+        ("5 moos", BookId::Deuteronomy),
+        ("5 mooses", BookId::Deuteronomy),
+        ("5. moos.", BookId::Deuteronomy),
         ("matt", BookId::Matthew),
         ("matt.", BookId::Matthew),
         ("matteus", BookId::Matthew),
